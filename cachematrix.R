b8e8ac3c7cf8a09cb9ce8ebac83cc1f05f5633e6
GitMeganJ <mjohnson928@yahoo.com>
2015-04-24 18:33:08 -0500

    Trying to submit my homework!

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to invert a marix.  If matrix was previously inverted, returns the result from prior calculation.
 
-## Write a short comment describing this function
+
+## This function makes a list of functions to calculate inversion.  
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setminv <- function(solve) m <<- solve
+    getminv <- function() m
+    list(set = set, get = get,
+         setminv = setminv,
+         getminv = getminv)  
+  
 }
 
 
-## Write a short comment describing this function
+## Looks for result from prior calculation.  If not found, inverts the matrix.  Returns the inverse x.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+      m <- x$getminv()
+      if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+      }
+      data <- x$get()
+      m <- solve(data, ...)
+      x$setminv(m)
+      m
 }
